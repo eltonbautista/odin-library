c978f722485d76b9254623b5a82befff3637b4fc
Elton Bautista <eltonbestaha@gmail.com>
2022-02-16 15:23:20 -0700

    I think I have found a way to create a book object and store it in myLibrary array. The only problem is I am not able to use myLibrary[i] to access them. I have to use the book's name.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-
+let book = {};
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -12,16 +12,27 @@ Book.prototype.info = function() {
     return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.read + '.';
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet'); 
+
+
 
 function addBookToLibrary() {
+
     let title = prompt('What is the book\'s title?', '');
     let author = prompt('Who is the author of the book?', '');
     let pages = prompt('How many pages is the book?', '');
     let read = prompt('Have you read the book?', '');
 
-let variableArr = [];
+    
     function objectName() {
-        
+        let someArr;
+        let variableObj;
+        someArr = title.split(' ');
+        variableObj = someArr[0][0].toLowerCase() + someArr[0].slice(1);
+
+        for (let i = 1; i < someArr.length; i++) {
+            variableObj = variableObj += (someArr[i][0].toUpperCase() + someArr[i].slice(1));
+        } return variableObj;
     } 
+return myLibrary[objectName()] = new Book(title, author, pages, read);
+
 }
